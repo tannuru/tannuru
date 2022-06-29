d745b199fd3ba098f878c552f52f417786a091df
tannuru <tilenbaevatanuka@gmail.com>
2022-06-29 09:40:50 +0600

    if else elif

---
+++ lesson1.py
@@ -0,0 +1,19 @@
+# list spisok kolleksii
+# a [1, 2, 3, 4]
+lists = [1, "jhdfv", 2.3, [1, 2], {"s: 3}"}, ]
+print(lists[3])
+
+#for i in range(10, 15, 2):
+    # print("hello")
+#    print(i)
+# #lists_1 = lists("str")
+# #print(lists_1)
+#
+# # pop()
+# #phone_numbers = [
+#     '+996507706009'
+#     ]
+# #phone_numbers.remove.('+996507706009')
+# #print(phone_numbers.count('=996507706009'))#phone_numbers.reverse()
+#
+# print("hello world: " 12, 16, 29, 31)
---
+++ lesson2.py
@@ -0,0 +1,49 @@
+
+#a =[2, 3, 4]
+#s = 'string'
+#a.reverse()
+#s.lower()
+#print(a)
+#print(s.lower())
+
+# if
+#num1 = int(input("num 1: "))
+#num2 = int(input("num 2: "))
+
+#if num1 > num2:
+    #print("{} grater {}".format(num1, num2))
+#elif num2 > num2:
+    #print("{0} grater {1}".format(num2, num1))
+
+# num1 = int(input("num 1: "))
+# num2 = int(input("num 2: "))
+# operator = input("operator: ")
+#
+# if operator == "+":
+#     result = num1 + num2
+#     print("result = {}".format(result))
+#
+# elif operator == "-":
+#     result = num1 - num2
+#     print("result = {}".format(result))
+#
+# elif operator  == "*":
+#     result = num1 * num2
+#     print("result = {}".format(result))
+#
+# elif operator == "/":
+#     result = num1 / num2
+#     print("result = {}".format(result))
+
+# lists = []
+# new_list = list(range(1, 1001))
+# for a in new_list:
+#     if a % 15 == 0:
+#         lists.append(a)
+# print(new_list)
+
+
+
+
+
+
